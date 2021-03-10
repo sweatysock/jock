85ca43a0917d564620e1febe8b5e2864d2011bef
sweatysock <mark@electricgt.co>
2021-03-10 10:32:52 +0000

    Fixed reading clientID & Secret from Heroku config vars

--- server.js
+++ server.js
@@ -49,11 +49,9 @@ const async = require('async');						// Used for asynchronous large file uploads
 //
 var clientID = "1cb0b9a5-058b-4224-a31e-7da7f1d82829";			// These are default values for testing
 var clientSecret = "1n3~PGlmw4xMpaz~hhmq12Na._V-Z9SZ97";
-console.log("clientID=",clientID," clientSecret=",clientSecret);
-if (process.env.clientID != undefined) clientID = process.env.clientID;	// If we are running in Heroku they should be set
-if (process.env.clientSecret != undefined) clientID = process.env.clientSecret;
-console.log("clientID=",clientID," clientSecret=",clientSecret);
-const scope = "offline_access files.readwrite";
+if (process.env.clientID != undefined) clientID = process.env.clientID;	// If we are running in Heroku these should be set
+if (process.env.clientSecret != undefined) clientSecret = process.env.clientSecret;
+const scope = "offline_access files.readwrite";				// Scope of access required for OneDrive
 var callback = "https://voicevault.herokuapp.com/authCallback";		// Authentication callback varies if on heroku
 if (PORT == undefined) callback = "https://localhost/authCallback";	// or running as localhost for testing
 accessToken = "";							// This needs to be valid to allow us to work with OneDrive
