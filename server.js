291fdf5ebb4705475d5be1ff8e19e522a40d6a15
sweatysock <mark@electricgt.co>
2021-04-14 19:03:03 +0100

    tracing authentication and callback as not working with new heroku instance

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get("/authcallback", function (req, res, next) {			// Microsoft will send th
 console.log("AUTH CALLBACK");
 console.log(req);
 	if (req.query.code === undefined) {				// Authentication didn't work. Display the error
-		res.status(200).send("OneDrive authorization error. Please contact VoiceVault support with this error:<br> "+decodeURIComponent(req.query.error));
+		res.status(200).send("OneDrive authorization error. Please contact VoiceVault support with this error:<br> "+decodeURIComponent(req.query.error_description));
 		next();
 	} else 	res.status(200).send("OneDrive authorization complete. You may close this window.");
 	console.log("OneDrive auth callback. code is "+req.query.code);
